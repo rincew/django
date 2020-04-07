3c4e8949880e18495d0cba4c4b2e082a80bbbeed
rincew <rincewi@gmail.com>
2020-04-07 17:28:04 +0000

    update

--- django/admin/settings.py
+++ django/admin/settings.py
@@ -67,6 +67,16 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 WSGI_APPLICATION = 'admin.wsgi.application'
 
 
